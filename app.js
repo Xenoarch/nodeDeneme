a1c1bfd0e1ea2ad0abe48be6b9ec44f0dd988911
Yiğit Çırak <yigitcirak@Yigit-Mac-mini.local>
2022-03-12 17:02:42 +0300

    variable port

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
 
+
+
 app.get('/', (req, res) => {
     res.send("Welcome home page");
 });
 
-app.listen(3000, () => { 
+const port = process.env.port || 3000;
+app.listen(port, () => { 
     console.log("Wazzaappp");
 });
 
