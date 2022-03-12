aae9d1e10a9e8a10e6c4057647f9cfaebfe3cd25
Yiğit Çırak <yigitcirak@Yigit-Mac-mini.local>
2022-03-12 18:00:54 +0300

    slash

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
     res.send("Welcome home page");
 });
 
+app.get('/musti', (req, res) => {
+    res.send("mustii");
+});
+
 const port = process.env.port || 3000;
 app.listen(port, () => { 
     console.log("Wazzaappp");
